a3a1c944425d1fa1ac65b91f11ba44df40047134
Dusha <nadezdafurman84@gmail.com>
2023-10-05 20:39:05 +0600

    Added solution Tasks

--- Homework_4/Task_1.py
+++ Homework_4/Task_1.py
@@ -1,2 +1,14 @@
 # Напишите функцию для транспонирования матрицы.
 # Пример: [[1, 2, 3], [4, 5, 6]] -> [[1,4], [2,5], [3, 6]]
+
+def trans_matrix(mat: list) -> list[int]:
+    return list(map(list, zip(*mat)))
+
+
+matrix = [[1, 2, 3], [4, 5, 6]]
+
+new_matrix = trans_matrix(matrix)
+
+print(*matrix, sep='\n')
+print()
+print(*new_matrix, sep='\n')
